8a4716ac3dafd1b3d3061df883759e876ed6b10a
artemscine <artempemine@gmail.com>
2024-03-28 13:39:13 +0300

    topaz: Drop Xiaomi Parts for now

--- device.mk
+++ device.mk
@@ -611,10 +611,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/xiaomi/topaz/topaz-vendor.mk)
 
